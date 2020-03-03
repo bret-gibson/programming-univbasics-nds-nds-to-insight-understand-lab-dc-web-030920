dd6680c07e2fda51255b44ad7d6be2ee74a58c72
Bret Gibson <bret.gibson@gmail.com>
2020-03-03 19:22:37 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -16,7 +16,7 @@ def print_first_directors_movie_titles
       column_index = 0
       while column_index < directors_database[row_index][:movies].length do
         puts "#{directors_database[row_index][:movies][column_index][:title]}\n"
-        #puts column_index
+        puts "blah"
         column_index += 1
       end
     end
