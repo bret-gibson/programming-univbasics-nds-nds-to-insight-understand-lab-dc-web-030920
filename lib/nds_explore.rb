f8135b9bbf8321a6fe57794548b3c3c9387cd802
Bret Gibson <bret.gibson@gmail.com>
2020-03-03 19:07:27 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,23 +11,12 @@ end
 def print_first_directors_movie_titles
   pretty_print_nds(directors_database)
   row_index = 0
-  while row_index < directors_database.length do
-    column_index = 0
-  while column_index < directors_database[row_index].length do
-    coord = "#{row_index}, #{column_index}"
-    inner_len = directors_database[row_index][column_index].length
+  while row_index == 0 do
+    puts directors_database[0][:name][:movies][element_index][:title]
+    #coord = "#{row_index}, #{column_index}"
+    #inner_len = directors_database[row_index][column_index].length
     # Remember \t is a TAB character for indentation
-    puts "\tCoordinate [#{coord}] points to an #{directors_database[row_index][column_index].class} of length #{inner_len}"
-
-    inner_index = 0
-    while inner_index < inner_len do
-      puts "\t\t (#{coord}, #{inner_index}) is: #{directors_database[row_index][column_index][inner_index]}"
-      inner_index += 1
-    end
-
-    column_index += 1
+    #puts "\tCoordinate [#{coord}] points to an #{directors_database[row_index][column_index].class} of length #{inner_len}"
+    row_index += 1
   end
-
-  row_index += 1
-end
 end
